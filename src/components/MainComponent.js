ccfc118a3ef08ddc902412f3d78e146199002dba
lalit6409 <mehtalalit6409@gmail.com>
2020-07-20 16:57:07 +0530

    lodash

--- src/components/MainComponent.js
+++ src/components/MainComponent.js
@@ -85,7 +85,7 @@ class Main extends Component {
                 <Header />
                 <Switch>
                     <Route path='/home' component={HomePage} />
-                    <Route exact path='/aboutus' component={() => <About leaders={this.props.leaders} />} />} />
+                    <Route exact path='/aboutus' component={() => <About leaders={this.props.leaders} />}  />
                     <Route exact path='/menu' component={() => <Menu dishes={this.props.dishes} />} />
                     <Route path='/menu/:dishId' component={DishWithId} />
                     <Route exact path='/contactus' component={() => <Contact resetFeedbackForm={this.props.resetFeedbackForm} />} />
