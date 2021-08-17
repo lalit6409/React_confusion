95d38f78db82ca7550695d9d7e3ee77db282acea
Lalit Kumar Mehta <lalit.mehta@tatadigital.com>
2021-08-17 22:31:48 +0530

    react animations components

--- src/components/DishdetailComponent.js
+++ src/components/DishdetailComponent.js
@@ -8,16 +8,23 @@ import { Control, LocalForm, Errors } from 'react-redux-form';
 import { Link } from 'react-router-dom';
 import { Loading } from './LoadingComponent';
 import { baseUrl } from '../shared/baseUrl';
+import { FadeTransform, Fade, Stagger } from 'react-animation-components';
 function RenderDish({ dish }) {
     if (dish != null) {
         return (
-            <Card >
+            <FadeTransform
+                in
+                transformProps={{
+                    exitTransform: 'scale(0.5) translateY(-50%)'
+                }}>
+            <Card>
                 <CardImg top src={baseUrl + dish.image} alt={dish.name} />
                 <CardBody>
                     <CardTitle>{dish.name}</CardTitle>
                     <CardText>{dish.description}</CardText>
                 </CardBody>
             </Card>
+            </FadeTransform>
         );
     }
     else
@@ -28,23 +35,35 @@ function RenderDish({ dish }) {
 function RenderComments({comments, postComment, dishId}) {
     if (comments != null) {
 
-        const com = comments.map((c) => {
-            return (
-                <div key={c.id} >
-                    <li>{c.comment}</li>
-                    <li>--{c.author}, {new Intl.DateTimeFormat('en-US', {
-                        year: 'numeric',
-                        month: 'short', // can be long  or short
-                        day: '2-digit'
-                    }).format(new Date(c.date))}</li>
-                </div>
-            );
-        });
+        // const com = comments.map((c) => {
+        //     return (
+        //         <div key={c.id} >
+        //             <li>{c.comment}</li>
+        //             <li>--{c.author}, {new Intl.DateTimeFormat('en-US', {
+        //                 year: 'numeric',
+        //                 month: 'short', // can be long  or short
+        //                 day: '2-digit'
+        //             }).format(new Date(c.date))}</li>
+        //         </div>
+        //     );
+        // });
         return (
             <div>
                 <h4>Comments</h4>
                 <ul className="list-unstyled">
-                    {com}
+                    <Stagger in>
+                        {comments.map((comment) => {
+                            return (
+                                <Fade in>
+                                <li key={comment.id}>
+                                <p>{comment.comment}</p>
+                                <p>-- {comment.author} , {new Intl.DateTimeFormat('en-US', { year: 'numeric', month: 'short', day: '2-digit'}).format(new Date(Date.parse(comment.date)))}</p>
+                                </li>
+                                </Fade>
+                            );
+                        })}
+                        </Stagger>
+                    {/* {com} */}
                 </ul>
                 <CommentForm dishId={dishId} postComment={postComment} />
             </div>
